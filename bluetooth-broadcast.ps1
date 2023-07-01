2ad6db9b51329992f1a9285eb98198e082d3bb3b
Tim Hämisch <71187051+haemtim@users.noreply.github.com>
2023-07-01 10:39:09 +0200

    Update bluetooth-broadcast.ps1

--- bluetooth-broadcast.ps1
+++ bluetooth-broadcast.ps1
@@ -1,9 +1,9 @@
-$hardwareID = 'BTHENUM\Dev_143FA690D5CD'
+$hardwareID = 'YOUR_DEVICE_ID_HERE' #SET DEVICE ID
 $devicefound = $true
 $device = Get-PnpDevice | Where-Object { $_.HardwareID -contains $hardwareID }
 while ($true) {
     while ($device.Status -ne "OK") {
-	    Start-Sleep -Seconds 5
+	    Start-Sleep -Seconds 5 #SET SLEEP
         $device = Get-PnpDevice | Where-Object { $_.HardwareID -contains $hardwareID }
         $devicefound = $true
     }
@@ -12,7 +12,7 @@ while ($true) {
         Write-Output "Headphones are connected!"
         Write-Output "Restarting Nvidia Broadcast..."
         Stop-Process -Name "NVIDIA Broadcast UI" -Force
-        Start-Process "C:\Program Files\NVIDIA Corporation\NVIDIA Broadcast\NVIDIA Broadcast UI.exe"
+        Start-Process "C:\Program Files\NVIDIA Corporation\NVIDIA Broadcast\NVIDIA Broadcast UI.exe" #SET PATH TO BROADCAST
         Write-Output "Nvidia Broadcast restarted!"
         Write-Output "-----------------------------"
     }
