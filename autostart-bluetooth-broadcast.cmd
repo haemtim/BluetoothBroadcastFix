a59f852d3b1f8446f47385bf2c10d2e2c8f5f11d
Tim Hämisch <71187051+haemtim@users.noreply.github.com>
2023-07-02 12:25:12 +0200

    fixed autostart-bluetooth-broadcast.cmd

--- autostart-bluetooth-broadcast.cmd
+++ autostart-bluetooth-broadcast.cmd
@@ -1,1 +1,5 @@
-powerShell -windowstyle hidden path\to\bluetooth-broadcast.ps1 >> C:\logs\StartupLog.log
+PowerShell -windowstyle hidden -Command "Set-ExecutionPolicy Unrestricted -Force" >> "%TEMP%\StartupLog.txt" 2>&1
+
+PowerShell -windowstyle hidden PATH\TO\bluetooth-broadcast.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+
+PowerShell -windowstyle hidden -Command "Set-ExecutionPolicy Restricted" >> "%TEMP%\StartupLog.txt" 2>&1
